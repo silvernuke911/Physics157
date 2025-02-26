19a6bf582a707e6466098e80edf3657788d75b8a
silvernuke911 <vercilbenben5@gmail.com>
2025-02-26 18:02:26 +0800

    decay!

---
+++ CH1/radioactive.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 3000
+p = 1/12
+t_max = 100
+t = np.arange(t_max)  # Use np.arange for proper indexing
+samples = np.ones(N)
+samplist = np.zeros(t_max)
+diffs = []
+samplist[0] = np.sum(samples)  # Set initial value
+for ti in range(1, t_max):  # Start from 1
+    difs = 0
+    for i in range(N):
+        choice = np.random.choice([0, 1], p=[p, 1 - p])
+        if samples[i] == 1 and choice == 0:
+            samples[i] = 0  
+            difs +=1
+    diffs.append(difs)
+    samplist[ti] = np.sum(samples)  
+    
+print(samplist)
+print(diffs)
+plt.step(t, samplist)
+plt.xlabel("Time Step")
+plt.ylabel("Number of Active Samples")
+plt.title("Decay Process Simulation")
+plt.show()
